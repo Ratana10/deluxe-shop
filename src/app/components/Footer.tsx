fa4b4c0361ee17535f5215cf734d9d0ff5251ea0
ratanawnk <sanratana18@gmail.com>
2024-09-05 13:16:56 +0700

    add instragram url

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -10,16 +10,20 @@ export default function Footer() {
         </div>
 
         <div className="mb-4 sm:mb-0 sm:w-1/3 text-center">
-          <p className="text-sm">
-            © Copyright 2024. All Rights Reserved.
-          </p>
+          <p className="text-sm">© Copyright 2024. All Rights Reserved.</p>
         </div>
 
         <div className="flex space-x-4 sm:w-1/3 justify-center sm:justify-end">
-          <Link href="https://www.facebook.com/profile.php?id=61561814585184" aria-label="Facebook">
+          <Link
+            href="https://www.facebook.com/profile.php?id=61561814585184"
+            aria-label="Facebook"
+          >
             <Facebook className="w-6 h-6 hover:text-gray-400" />
           </Link>
-          <Link href="https://instagram.com" aria-label="Instagram">
+          <Link
+            href="https://www.instagram.com/de.luxeglam0ur/"
+            aria-label="Instagram"
+          >
             <Instagram className="w-6 h-6 hover:text-gray-400" />
           </Link>
         </div>
---
+++ src/app/contactus/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactUsPage = () => {
+  return (
+    <div>
+      {/* Social Media Link */}
+      
+    </div>
+  )
+}
+
+export default ContactUsPage
