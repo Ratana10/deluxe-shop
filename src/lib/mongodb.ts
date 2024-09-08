e6212ae6a5e73cf393bd547c4a28bf0ed582d7df
ratanawnk <sanratana18@gmail.com>
2024-09-08 14:50:27 +0700

    update mongodb uri

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,21 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-
-const MONGODB_URI = process.env.MONGODB_URI;
-
+// const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = "mongodb://127.0.0.1:27017/db_deluxe";
 
 // check the MongoDB URI
 if (!MONGODB_URI) {
   throw new Error("Define the MONGODB_URI environmental variable");
 }
 
-
-
 export const connectMongoDB = async () => {
   try {
-   await  mongoose.connect(MONGODB_URI);
-    console.log("connect to mongodb")
+    await mongoose.connect(MONGODB_URI);
+    console.log("connect to mongodb");
   } catch (error: any) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
